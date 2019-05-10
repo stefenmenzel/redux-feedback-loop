0f3ba2461d0e759712ec559e591b660a591cb728
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-05-10 17:14:38 -0500

    made a component that works for the first three pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
+
+import ScaleForm from '../ScaleForm/ScaleForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+          {/* <FeelingForm /> */}
+          <Route exact path='/'
+            render={props => (<ScaleForm formType={this.props.reduxState.feeling} passedProps={props} />)}
+          />
+          <Route exact path='/understanding'
+            render={props => (<ScaleForm formType={this.props.reduxState.understanding} passedProps={props} />)}
+          />
+          <Route exact path='/support'
+            render={props => (<ScaleForm formType={this.props.reduxState.support} passedProps={props} />)}
+          />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (reduxState) => {
+  return {reduxState};
+}
+
+export default connect(mapStateToProps)(App);
